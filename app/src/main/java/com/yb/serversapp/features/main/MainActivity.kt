77443d7ef72d0301dad02e8d3ebe3d9d4bc1a0df
Yogesh Buradkar <yogesh.buradkar@gmail.com>
2020-10-16 20:45:57 +0100

    fetch and confirm api response

--- app/src/main/java/com/yb/serversapp/features/main/MainActivity.kt
+++ app/src/main/java/com/yb/serversapp/features/main/MainActivity.kt
@@ -1,12 +1,37 @@
 package com.yb.serversapp.features.main
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.yb.serversapp.R
+import android.view.LayoutInflater
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import com.yb.serversapp.App
+import com.yb.serversapp.databinding.ActivityMainBinding
+import com.yb.serversapp.di.ViewModelFactory
+import timber.log.Timber
+import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    @Inject lateinit var viewModelFactory: ViewModelFactory
+    private val viewModel: MainViewModel by viewModels { viewModelFactory }
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        inject()
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(LayoutInflater.from(this))
+        setContentView(binding.root)
+        observeViewModel()
     }
+
+    private fun inject() {
+        (application as App).appComponent.inject(this)
+    }
+
+    private fun observeViewModel() {
+        viewModel.serverStatuses.observe(this) {
+            Timber.d("Resource status = ${it.status}")
+        }
+    }
+
 }
