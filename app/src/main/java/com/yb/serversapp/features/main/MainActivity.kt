af4f2904382e26d6e67bdd9a7a7903f7678e7681
Yogesh Buradkar <yogesh.buradkar@gmail.com>
2020-10-17 21:30:55 +0100

    add action bar titles and handle the up button

--- app/src/main/java/com/yb/serversapp/features/main/MainActivity.kt
+++ app/src/main/java/com/yb/serversapp/features/main/MainActivity.kt
@@ -3,16 +3,33 @@ package com.yb.serversapp.features.main
 import android.os.Bundle
 import android.view.LayoutInflater
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.yb.serversapp.R
 import com.yb.serversapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private val navController: NavController by lazy { findNavController(R.id.nav_host_fragment) }
+    private val appBarConfiguration by lazy { AppBarConfiguration(navController.graph) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(LayoutInflater.from(this))
         setContentView(binding.root)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 
 }
