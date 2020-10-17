e210c28da46d9320b08d9027cfd52cd9107d14f8
Yogesh Buradkar <yogesh.buradkar@gmail.com>
2020-10-17 20:58:08 +0100

    set up navigation components for navigation to detail fragment

--- app/src/main/java/com/yb/serversapp/features/main/MainActivity.kt
+++ app/src/main/java/com/yb/serversapp/features/main/MainActivity.kt
@@ -2,17 +2,10 @@ package com.yb.serversapp.features.main
 
 import android.os.Bundle
 import android.view.LayoutInflater
-import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.commit
 import com.yb.serversapp.App
-import com.yb.serversapp.R
 import com.yb.serversapp.databinding.ActivityMainBinding
 import com.yb.serversapp.di.ViewModelFactory
-import com.yb.serversapp.features.status.ServersFragment
-import com.yb.serversapp.features.status.ServersViewModel
-import timber.log.Timber
 import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
@@ -25,22 +18,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(LayoutInflater.from(this))
         setContentView(binding.root)
-        if(savedInstanceState == null) switchFragment(ServersFragment.newInstance(), false)
-    }
-
-    private fun switchFragment(fragment: Fragment, addToBackStack: Boolean) {
-        supportFragmentManager.commit(allowStateLoss = false) {
-            replace(R.id.frag_container, fragment, CURRENT_FRAGMENT_TAG)
-            if(addToBackStack) addToBackStack(null)
-        }
     }
 
     private fun inject() {
         (application as App).appComponent.inject(this)
-    }
-
-    companion object {
-        private const val CURRENT_FRAGMENT_TAG = "current_fragment"
     }
 
 }
--- app/src/main/java/com/yb/serversapp/features/status/ServersAdapter.kt
+++ app/src/main/java/com/yb/serversapp/features/status/ServersAdapter.kt
@@ -6,7 +6,6 @@ import androidx.recyclerview.widget.AsyncDifferConfig
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import com.yb.serversapp.R
 import com.yb.serversapp.databinding.ItemServerBinding
 import com.yb.serversapp.databinding.ItemServerTypeBinding
 import com.yb.serversapp.domain.models.ServerStatus
@@ -15,19 +14,22 @@ import com.yb.serversapp.features.status.ServersAdapter.ServerViewItem.ServerSta
 import com.yb.serversapp.features.status.ServersAdapter.ServerViewItem.ServerTypeItem
 import java.util.*
 
-class ServersAdapter: ListAdapter<ServerViewItem, RecyclerView.ViewHolder>(
+class ServersAdapter(
+    private val serverClickAction: (UiServerStatus) -> Unit
+) : ListAdapter<ServerViewItem, RecyclerView.ViewHolder>(
     AsyncDifferConfig.Builder(DIFF_CALLBACK).build()
 ) {
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        return when(viewType) {
+        return when (viewType) {
             SERVER_TYPE_VIEWTYPE -> ServerTypeViewHolder.create(parent)
-            SERVER_STATUS_VIEWTYPE -> ServerStatusViewHolder.create(parent)
+            SERVER_STATUS_VIEWTYPE -> ServerStatusViewHolder.create(parent, serverClickAction)
             else -> throw Exception("Unknown view type")
         }
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        when(val item = getItem(position)) {
+        when (val item = getItem(position)) {
             is ServerTypeItem -> (holder as ServerTypeViewHolder).bind(item)
             is ServerStatusItem -> (holder as ServerStatusViewHolder).bind(item)
         }
@@ -35,12 +37,13 @@ class ServersAdapter: ListAdapter<ServerViewItem, RecyclerView.ViewHolder>(
 
     override fun getItemViewType(position: Int): Int = getItem(position).viewType
 
-    class ServerTypeViewHolder(private val binding: ItemServerTypeBinding): RecyclerView.ViewHolder(binding.root) {
+    class ServerTypeViewHolder(private val binding: ItemServerTypeBinding) : RecyclerView.ViewHolder(binding.root) {
         fun bind(item: ServerTypeItem) {
             with(binding) {
                 serverType.text = item.serverType
             }
         }
+
         companion object {
             fun create(parent: ViewGroup): ServerTypeViewHolder {
                 return ServerTypeViewHolder(ItemServerTypeBinding.inflate(LayoutInflater.from(parent.context), parent, false))
@@ -48,24 +51,31 @@ class ServersAdapter: ListAdapter<ServerViewItem, RecyclerView.ViewHolder>(
         }
     }
 
-    class ServerStatusViewHolder(private val binding: ItemServerBinding): RecyclerView.ViewHolder(binding.root) {
+    class ServerStatusViewHolder(
+        private val binding: ItemServerBinding,
+        private val serverClickAction: (UiServerStatus) -> Unit
+    ) : RecyclerView.ViewHolder(binding.root) {
         fun bind(item: ServerStatusItem) {
             with(binding) {
                 status.text = item.status.responseCode?.toString()
                 status.setBackgroundResource(item.status.getStatusColor())
                 serverName.text = item.status.serverName
+                root.setOnClickListener { serverClickAction.invoke(item.status) }
             }
         }
 
         companion object {
-            fun create(parent: ViewGroup): ServerStatusViewHolder {
-                return ServerStatusViewHolder(ItemServerBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+            fun create(parent: ViewGroup, serverClickAction: (UiServerStatus) -> Unit): ServerStatusViewHolder {
+                return ServerStatusViewHolder(
+                    binding = ItemServerBinding.inflate(LayoutInflater.from(parent.context), parent, false),
+                    serverClickAction = serverClickAction
+                )
             }
         }
     }
 
     sealed class ServerViewItem(val viewType: Int, val id: String = UUID.randomUUID().toString()) {
-        data class ServerTypeItem(val serverType: String): ServerViewItem(SERVER_TYPE_VIEWTYPE)
+        data class ServerTypeItem(val serverType: String) : ServerViewItem(SERVER_TYPE_VIEWTYPE)
 
         data class ServerStatusItem(val status: UiServerStatus) : ServerViewItem(SERVER_STATUS_VIEWTYPE)
 
@@ -73,8 +83,8 @@ class ServersAdapter: ListAdapter<ServerViewItem, RecyclerView.ViewHolder>(
             fun from(statuses: List<ServerStatus>): List<ServerViewItem> {
                 val items = mutableListOf<ServerViewItem>()
                 var prevType = ""
-                for(status in statuses) {
-                    if(prevType != status.type) {
+                for (status in statuses) {
+                    if (prevType != status.type) {
                         items.add(ServerTypeItem(serverType = status.type))
                         prevType = status.type
                     }
